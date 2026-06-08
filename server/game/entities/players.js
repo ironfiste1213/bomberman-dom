23e2ce13b5e4b68ed21c833c138bf77eb7a46310
ironfiste1213 <mimouneelgamoussi2@gmail.com>
2026-06-08 15:02:34 +0100

    backend: update the player class , the subject requires 3lives , bomb capacity, flam range and speed powerup

--- server/game/entities/players.js
+++ server/game/entities/players.js
@@ -4,9 +4,11 @@ export default class Player {
 
         this.x = 0;
         this.y = 0;
-
-        this.speed = 1;
-
+        this.speed = 0.1;
+        this.lives = 3;
+        this.bombLimit = 1;
+        this.activeBombs = 0;
+        this.flagRange = 1;
         this.input = {
             up: false,
             down: false,
