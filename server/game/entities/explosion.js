763d0e55c52d1a14833b600a58af2d9d470ef753
melgamous <mimouneelgamoussi2@gmail.com>
2026-06-13 14:08:20 +0100

    backend entities add powerUp, bomb, explosion , and block

---
+++ server/game/entities/explosion.js
@@ -0,0 +1,8 @@
+export default class Explosion {
+    constructor(id, x, y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.duration = 300; // milliseconds
+    }
+}
